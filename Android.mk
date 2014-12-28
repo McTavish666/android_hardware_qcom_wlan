1fb72ea8bf55e11c7198c14faebc09d858c55bc2
Steve Kondik <steve@cyngn.com>
2014-12-28 02:32:53 -0800

    wlan-caf: Add guard makefile
    Change-Id: I79aacfe2438c0e871cb5a61156524c0de3cb540e

--- Android.mk
+++ Android.mk
@@ -1,4 +1,7 @@
+ifeq ($(call my-dir),$(call project-path-for,wlan))
 # TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
 ifneq ($(BOARD_IS_AUTOMOTIVE),true)
 include $(call all-subdir-makefiles)
+
+endif
 endif
